64005c05b6068e9f8b4396d3a54019654a05849f
dej611 <dej611@gmail.com>
2021-03-28 16:04:10 +0200

    :bug: script fix

--- scripts/styles.sh
+++ scripts/styles.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cp ./node_modules/spid-smart-button/dist/spid-button.min.css ./src/modalVariant/index.module.css
-if [ uname = 'Linux' ]; then
+if [ $(uname) = 'Linux' ]; then
     sed -i '/@import url/d' ./src/modalVariant/index.module.css
 else 
     sed -i '' '/@import url/d' ./src/modalVariant/index.module.css
