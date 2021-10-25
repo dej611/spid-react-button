f64d1bc36f488e34ca3c74522204ad711c881d3e
Catriel Müller <catrielmuller@gmail.com>
2021-10-25 04:26:21 -0300

    feat: [#65] French translation (#70)

--- src/shared/i18n.ts
+++ src/shared/i18n.ts
@@ -3,67 +3,78 @@ const translations = {
     it: 'Torna indietro',
     en: 'Go back',
     de: 'Gehen Sie zurück',
-    es: 'Regresar'
+    es: 'Regresar',
+    fr: 'Revenir'
   },
   alt_logo_SPID: {
     it: 'Logo SPID',
     en: 'Logo SPID',
     de: 'Logo SPID',
     es: 'Logo SPID',
+    fr: 'Logo SPID'
   },
   scegli_provider_SPID: {
     it: 'Scegli il tuo provider SPID',
     en: 'Choose your SPID provider',
     de: 'Wähle Ihren SPIDProvider',
-    es: 'Escoge tu proveedor SPID'
+    es: 'Escoge tu proveedor SPID',
+    fr: 'Choisissez votre fournisseur SPID'
   },
   annulla_accesso: {
     it: 'Annulla',
     en: 'Cancel',
     de: 'Zurücknehmen',
-    es: 'Cancelar'
+    es: 'Cancelar',
+    fr: 'Annuler'
   },
   non_hai_SPID: {
     it: 'Non hai SPID?',
     en: "Don't have SPID?",
     de: 'Haben Sie nicht SPID?',
-    es: 'No tiene SPID?'
+    es: 'No tiene SPID?',
+    fr: "Vous ñ'avez pas de SPID?"
   },
-  cosa_SPID: { 
+  cosa_SPID: {
     it: "Cos'è SPID?",
     en: 'What is SPID?',
     de: 'Was ist SPID?',
-    es: 'Qué es SPID?'
+    es: 'Qué es SPID?',
+    fr: 'Qu’est-ce que SPID?'
   },
   entra_con_SPID: {
     it: 'Entra con SPID',
     en: 'Sign in with SPID',
     de: 'Loggen Sie mit SPID',
-    es: 'Ingresa con SPID'
+    es: 'Ingresa con SPID',
+    fr: 'Connectez-vous avec SPID'
   },
   scopri_di_piu: {
     it: 'Scopri di più.',
     en: 'Learn more.',
     de: 'Finde mehr heraus.',
-    es: 'Saber más'
+    es: 'Saber más',
+    fr: 'En savoir plus.'
   },
   accedi_con_idp: {
     it: 'Accedi a SPID con {0}',
     en: 'Access to SPID with {0}',
     de: 'Zugriff auf SPID mit {0}',
-    es: 'Accede a SPID con {0}'
+    es: 'Accede a SPID con {0}',
+    fr: 'Accès à SPID avec {0}'
   },
   idp_disabled: {
     it: 'Provider non attivo',
     en: 'Provider not enabled',
     de: 'Provider nicht aktiviert',
-    es: 'Proveedor no disponible'
+    es: 'Proveedor no disponible',
+    fr: 'Fournisseur non activé'
   },
   maggiori_info: {
     it: 'Maggiori info',
     en: 'More info',
     de: 'Mehr info',
-    es: 'Más información'
+    es: 'Más información',
+    fr: 'Plus d’info'
   }
 } as const;
 
