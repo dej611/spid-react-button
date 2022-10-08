8d82234967a6f8414d5f93f0da761c47bd4d6042
dej611 <dej611@gmail.com>
2022-10-08 12:45:52 +0200

    :wrench: Fix for vercel

--- scripts/svgs.sh
+++ scripts/svgs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-mkdir temp
-curl -L  "https://github.com/italia/spid-sp-access-button/archive/master.tar.gz" | tar -C temp --strip-components=4 -xf - spid-sp-access-button-master/src/production/img/spid-idp*.svg
+mkdir /tmp/idp-svgs
+curl -L  "https://github.com/italia/spid-sp-access-button/archive/master.tar.gz" | tar -C /tmp/idp-svgs --strip-components=4 -xf - spid-sp-access-button-master/src/production/img/spid-idp*.svg
 
 npx svgo -f node_modules/spid-smart-button/dist/img/ -o src/shared/svgs
-npx svgo -f temp/ -o src/shared/svgs/idp-logos
+npx svgo -f /tmp/idp-svgs -o src/shared/svgs/idp-logos
 
-rm -r temp
+rm -r /tmp/idp-svgs
