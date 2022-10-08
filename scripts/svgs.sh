dea6b8a8d0d5c4fe680169a229042675cdbdd349
dej611 <dej611@gmail.com>
2022-10-08 12:26:17 +0200

    :wrench: Pull providers assets from agid repo

--- scripts/svgs.sh
+++ scripts/svgs.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+mkdir temp
+curl -L  "https://github.com/italia/spid-sp-access-button/archive/master.tar.gz" | tar -C temp --strip-components=4 -xf - spid-sp-access-button-master/src/production/img/spid-idp*.svg
 
 npx svgo -f node_modules/spid-smart-button/dist/img/ -o src/shared/svgs
-npx svgo -f node_modules/spid-smart-button/dist/img/idp-logos/ -o src/shared/svgs/idp-logos
+npx svgo -f temp/ -o src/shared/svgs/idp-logos
+
+rm -r temp
