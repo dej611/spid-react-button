c674b768c12307bc7c2fda5f513e6b1187ecbd84
Marco Liberati <dej611@users.noreply.github.com>
2022-10-08 15:14:18 +0200

    Update svgs.sh
    Add z flag

--- scripts/svgs.sh
+++ scripts/svgs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir /tmp/idp-svgs
-curl -L  "https://github.com/italia/spid-sp-access-button/archive/master.tar.gz" | tar -C /tmp/idp-svgs --strip-components=4 -xf - spid-sp-access-button-master/src/production/img/spid-idp*.svg
+curl -L  "https://github.com/italia/spid-sp-access-button/archive/master.tar.gz" | tar -C /tmp/idp-svgs --strip-components=4 -zxf - spid-sp-access-button-master/src/production/img/spid-idp*.svg
 
 npx svgo -f node_modules/spid-smart-button/dist/img/ -o src/shared/svgs
 npx svgo -f /tmp/idp-svgs -o src/shared/svgs/idp-logos
